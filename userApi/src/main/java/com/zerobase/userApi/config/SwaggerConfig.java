b2e42e168ed64d81ccca99e1e6dd110f872af298
jameskim9509 <wltns1282@gmail.com>
2024-07-09 16:21:19 +0900

    swagger ui 적용 및 revisionHistory 테이블 적용

---
+++ userApi/src/main/java/com/zerobase/userApi/config/SwaggerConfig.java
@@ -0,0 +1,24 @@
+package com.zerobase.userApi.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+    @Bean
+    public OpenAPI openAPI() {
+        return new OpenAPI()
+                .components(new Components())
+                .info(apiInfo());
+    }
+
+    private Info apiInfo() {
+        return new Info()
+                .title("USER REST API Specifications")
+                .description("Manage Customer and Seller by REST API")
+                .version("1.0.0");
+    }
+}
