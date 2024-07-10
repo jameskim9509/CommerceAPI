12d788594d473cba096aec2c77e28c590c7bf531
jameskim9509 <wltns1282@gmail.com>
2024-07-10 23:46:37 +0900

    상품 및 상품아이템 삭제기능 구현 및 테스트코드 작성

--- orderApi/src/main/java/com/zerobase/orderApi/repository/ProductRepository.java
+++ orderApi/src/main/java/com/zerobase/orderApi/repository/ProductRepository.java
@@ -1,13 +1,19 @@
 package com.zerobase.orderApi.repository;
 
 import com.zerobase.orderApi.domain.Product;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
 @Repository
 public interface ProductRepository extends JpaRepository<Product, Long> {
+    @EntityGraph(attributePaths = {"productItemList"})
     Optional<Product> findBySellerId(Long sellerId);
+
+    @EntityGraph(attributePaths = {"productItemList"})
     Optional<Product> findBySellerIdAndId(Long sellerId, Long id);
 }
